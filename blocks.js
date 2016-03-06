a73ab9306535662ac4885b4c51651a33e0ca34a9
guterres <guterres25@gmail.com>
2016-03-06 07:22:18 +0000

    dynamics route instance

--- blocks.js
+++ blocks.js
@@ -19,33 +19,39 @@ app.param('name', function(request, response, next){ //called for routes which i
   next(); //must be called to resume request
 });
 
-app.get('/blocks', function(request, response){
-  if(request.query.limit >= 0){ //true when a numeric value for limit is part of the URL
-    response.json(Object.keys(blocos).slice(0, request.query.limit));
-  }else{
-    response.json(Object.keys(blocos)); //returns all results
-  }
-});
+app.route('/blocks') //dynamic route instance
+  .get(function(request, response){
+    if(request.query.limit >= 0){ //true when a numeric value for limit is part of the URL
+      response.json(Object.keys(blocos).slice(0, request.query.limit));
+    }else{
+      response.json(Object.keys(blocos)); //returns all results
+    }
+  })
 
-app.get('/blocks/:name', function(request, response){
-  var description = blocos[request.blockName];
-  if(description){
-    response.json(description);
-  }else{
-    response.status(404).json('No description found for '+request.params.name);
-  }
-});
+  .post(parseUrlencoded, function(request, response){
+    var newBlock = request.body; //returns form data
+    blocos[newBlock.name] = newBlock.description; //adds new block to the blocks Object
 
-app.post('/blocks', parseUrlencoded, function(request, response){
-  var newBlock = request.body; //returns form data
-  blocos[newBlock.name] = newBlock.description; //adds new block to the blocks Object
+    response.status(201).json(newBlock.name); //responds with new block name with sets the 201 created status code
+  });
 
-  response.status(201).json(newBlock.name); //responds with new block name with sets the 201 created status code
-});
+app.route('/blocks/:name')
+  .get(function(request, response){
+    var description = blocos[request.blockName];
+    if(description){
+      response.json(description);
+    }else{
+      response.status(404).json('No description found for '+request.params.name);
+    }
+  })
+  .delete(function(request, response){
+    if(blocos[request.blockName]){
+      delete blocos[request.blockName];
+      response.sendStatus(200);
+    }else{
+      response.sendStatus(404);
+    }
 
-app.delete('/blocks/:name', function(request, response){
-  delete blocos[request.blockName];
-  response.sendStatus(200);
-});
+  });
 
 app.listen(3000);
